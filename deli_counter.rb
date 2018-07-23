0f789e3ce33d0b39363cf651d4e2829680db32d3
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 04:34:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(array)
+  string = 'The line is currently: '
+
+  if array.size == 0
+    puts 'The line is currently empty.'
+    return
+  end
+
+  counter = 0
+  while counter < array.size - 1
+    array.each {|index| string += "#{array.index(index) + 1}. #{index} "}
+    counter += 1
+    if counter == array.size -1
+      break
+    end
+  end
+  puts string
+end
