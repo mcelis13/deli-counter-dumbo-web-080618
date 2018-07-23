0e218fc225c69946b69fe47f7441ab5557695fe5
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 04:53:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  array.each {|index| puts "Welcome, #{name}. You are number #{array.size} in line."}
+  puts "Welcome, #{name}. You are number #{array.size} in line."
 end
 
 def now_serving()
