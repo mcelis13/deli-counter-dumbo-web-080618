40e94619e102206271fc320cbc138a4d197115fd
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 04:46:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ end
 
 num = 0;
 def take_a_number(array, name)
-  array.each {|index| puts "Welcome, #{index}. You are #{array.index(index) + 1} in line."};
+  array.each {|index| katz_deli.push(index) puts "Welcome, #{index}. You are #{array.index(index) + 1} in line."};
 end
 
 def now_serving()
