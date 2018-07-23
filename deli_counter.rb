c9304fb5745ecc2f3cc3e8fa31deedd0cb78969e
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 04:49:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,6 @@ num = 0;
 def take_a_number(array, name)
   katz_deli.push(name)
   array.each {|index| puts "Welcome, #{index}. You are #{array.index(index) + 1} in line."}
-  return katz_deli
 end
 
 def now_serving()
