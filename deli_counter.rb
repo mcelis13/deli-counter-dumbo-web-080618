ac821d8440d203a31c2184f46d422f06d8790db8
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 04:56:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,12 +10,13 @@ end
 
 def take_a_number(array, name)
   array.push(name)
+  
   if array.size === 0
     puts "The line is currently empty."
   else
   puts "Welcome, #{name}. You are number #{array.size} in line."
   end
-  
+
 end
 
 def now_serving()
