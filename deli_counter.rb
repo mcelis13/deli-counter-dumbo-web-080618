6f559a24eb6af48831040465c3a4416d6d150d23
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 04:37:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,16 +5,6 @@ def line(array)
     puts 'The line is currently empty.'
     return
   end
-
-  counter = 0
-  while counter < array.size - 1
-    array.each {|index| string += "#{array.index(index) + 1}. #{index} "}
-    counter += 1
-    if counter == array.size -1
-      break
-    end
-  end
-  puts string
 end
 
 def take_a_number(array, name)
